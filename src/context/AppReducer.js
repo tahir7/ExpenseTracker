fd57cdd01e113414cd2be58de7584d5a7e1f6d56
tahir7 <tabbas2005@yahoo.com>
2020-06-19 06:19:02 -0400

    Expense Tracker

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -10,7 +10,7 @@ export default (state, action)  => {
     switch(action.type) {
         case 'DELETE_TRANSACTION':
             return {
-               ...state,
+               ...state, 
                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
             }
         
